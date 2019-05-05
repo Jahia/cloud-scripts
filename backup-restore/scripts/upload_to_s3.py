ca78bb73f84b83c9817e3b2c2d819ea936ee95d9
julian-maurel <jmaurel@jahia.com>
2019-05-05 09:17:17 +0200

    PAAS-199: enhanced backup init

---
+++ backup-restore/scripts/upload_to_s3.py
@@ -0,0 +1,53 @@
+import logging
+import boto3
+import sys
+import os
+import threading
+from botocore.exceptions import ClientError
+
+
+
+class ProgressPercentage(object):
+
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+        
+    # Upload the file
+    s3_client = boto3.client('s3', aws_access_key_id=sys.argv[2],
+         aws_secret_access_key=sys.argv[3])
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name, Callback=ProgressPercentage(file_name))
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+print ("uploading file " + sys.argv[1])
+upload_file (sys.argv[1], 'paas-environment-backup', sys.argv[1])
